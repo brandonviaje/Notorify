81d60f2264e25b29857f3a252435c1112f591202
brandonviaje <brandon.viaje@ontariotechu.net>
2025-06-12 23:29:56 -0400

    feat: add JWT for user auth

---
+++ NotorifyAPI/src/main/java/com/notorify/notorifyapi/Service/JwtService.java
@@ -0,0 +1,15 @@
+package com.notorify.notorifyapi.Service;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public interface JwtService {
+    public String extractUserName(String token);
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver);
+    public String generateToken(UserDetails userDetails);
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails);
+    public long getExpirationTime();
+}
