878bb3dba2c4e30a05d9ea1c26a4244838eaf06d
brandonviaje <brandon.viaje@ontariotechu.net>
2025-06-12 22:58:00 -0400

    chore: set up project structure, env variables

---
+++ NotorifyAPI/src/main/java/com/notorify/notorifyapi/NotorifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.notorify.notorifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotorifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotorifyApiApplication.class, args);
+	}
+
+}
---
+++ NotorifyAPI/src/test/java/com/notorify/notorifyapi/NotorifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notorify.notorifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotorifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
